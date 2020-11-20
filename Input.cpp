e85a86d7db0b0fb11f64c52845329070b5fd646c
Toby <sukoshijon@gmail.com>
2020-11-20 12:55:58 +0000

    basic keyboard input

--- Input.cpp
+++ Input.cpp
@@ -1,33 +1,41 @@
 #include "Input.h"
 
-namespace SouthwestEngine {
-	SDL_Joystick* Input::_joystick;
+using namespace SouthwestEngine;
 
-	void Input::Update() {
-		if (SDL_NumJoysticks() > 0) {
-			if (_joystick == nullptr) {
-				_joystick = SDL_JoystickOpen(0);
-			}
-			SDL_JoystickUpdate();
-		}
-		else {
-			// close joystick object if joystick is disconnected. this might be unnecessary i dont know if SDL does this for you
-			if (_joystick != nullptr) {
-				SDL_JoystickClose(_joystick);
-				_joystick = nullptr;
-			}
-		}
+SDL_Joystick* Input::_joystick;
+const Uint8* Input::__keyboard;
 
-		// events
-		SDL_Event e;
-		while (SDL_PollEvent(&e)) {
-			switch (e.type) {
-			case SDL_QUIT:
-				Southwest::Running = false;
-				break;
-			default:
-				break;
-			}
+void Input::Update() {
+	if (SDL_NumJoysticks() > 0) {
+		if (_joystick == nullptr) {
+			_joystick = SDL_JoystickOpen(0);
+		}
+		SDL_JoystickUpdate();
+	}
+	else {
+		// close joystick object if joystick is disconnected. this might be unnecessary i dont know if SDL does this for you
+		if (_joystick != nullptr) {
+			SDL_JoystickClose(_joystick);
+			_joystick = nullptr;
 		}
 	}
+
+	// events
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+		switch (e.type) {
+		case SDL_QUIT:
+			Southwest::Running = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	__SDL_Keyboard = SDL_GetKeyboardState(nullptr);
+
+}
+
+bool Input::Key(SDL_Scancode key) {
+	return (bool)__SDL_Keyboard[key];
 }
--- Input.h
+++ Input.h
@@ -12,8 +12,17 @@ namespace SouthwestEngine {
 		Input();
 
 		static SDL_Joystick* _joystick;
+
+		static const Uint8* __SDL_Keyboard;
+		static const Uint8* __keyboard;
+		static const Uint8* __keyboardChange;
 	public:
 		static void Update();
+
+		// keyboard input
+		static bool KeyDown(SDL_Scancode key);
+		static bool KeyUp(SDL_Scancode key);
+		static bool Key(SDL_Scancode key);
 	};
 }
 
