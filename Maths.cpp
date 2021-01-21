66790bd15f68adadad842b8b4a7846d33bfc2a8f
BromBromBromley <tonymichaeltajiri@gmail.com>
2021-01-21 18:06:18 -0500

    Added MoveTowards() functions

--- Maths.cpp
+++ Maths.cpp
@@ -1,4 +1,6 @@
 #include "Maths.h"
+#include <OGRE/OgreVector.h>
+#include <cmath>
 
 using namespace SouthwestEngine;
 
@@ -8,4 +10,12 @@ double Math::Lerp(double v0, double v1, double t) {
 
 double Math::Cubic(double v[4], double t) {
 	return v[1] + 0.5 * t * (v[2] - v[0] + t * (2.0 * v[0] - 5.0 * v[1] + 4.0 * v[2] - v[3] + t * (3.0 * (v[1] - v[2]) + v[3] - v[0])));
+}
+
+double Math::MoveToward(double p_from, double p_to, double p_delta) {
+	return std::abs(p_to - p_from) <= p_delta ? p_to : p_from + (p_to - p_from) * p_delta;
+}
+
+float Math::MoveToward(float p_from, float p_to, float p_delta) {
+	return std::abs(p_to - p_from) <= p_delta ? p_to : p_from + (p_to - p_from) * p_delta;
 }
--- Maths.h
+++ Maths.h
@@ -9,6 +9,9 @@ namespace SouthwestEngine {
 	public:
 		static double Lerp(double v0, double v1, double t);
 		static double Cubic(double v[4], double t);
+		static double MoveToward(double p_from, double p_to, double p_delta);
+		static float MoveToward(float p_from, float p_to, float p_delta);
+		// TODO - Add Ogre::Vector3 overloads
 	};
 }
 
