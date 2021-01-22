19314380fadea7ab7ab69723123455738768426a
BromBromBromley <tonymichaeltajiri@gmail.com>
2021-01-21 21:30:33 -0500

    Added additional Vector3 helper functions

--- Maths.cpp
+++ Maths.cpp
@@ -1,6 +1,4 @@
 #include "Maths.h"
-#include <OGRE/OgreVector.h>
-#include <cmath>
 
 using namespace SouthwestEngine;
 
@@ -12,10 +10,26 @@ double Math::Cubic(double v[4], double t) {
 	return v[1] + 0.5 * t * (v[2] - v[0] + t * (2.0 * v[0] - 5.0 * v[1] + 4.0 * v[2] - v[3] + t * (3.0 * (v[1] - v[2]) + v[3] - v[0])));
 }
 
+float Math::Distance(const Ogre::Vector3& p_from, const Ogre::Vector3& p_to)
+{
+	return (p_to - p_from).length();
+}
+
 double Math::MoveToward(double p_from, double p_to, double p_delta) {
 	return std::abs(p_to - p_from) <= p_delta ? p_to : p_from + (p_to - p_from) * p_delta;
 }
 
 float Math::MoveToward(float p_from, float p_to, float p_delta) {
 	return std::abs(p_to - p_from) <= p_delta ? p_to : p_from + (p_to - p_from) * p_delta;
+}
+
+Ogre::Vector3 Math::MoveToward(const Ogre::Vector3& p_from, const Ogre::Vector3& p_to, const float p_delta) {
+	Ogre::Vector3 distance = p_to - p_from;
+	float length = distance.length();
+	return length <= p_delta || length < CMP_EPSILON ? p_to : p_from + distance / length * p_delta;
+}
+
+Ogre::Vector3 Math::Abs(const Ogre::Vector3& vec3)
+{
+	return Ogre::Vector3(std::abs(vec3.x), std::abs(vec3.y), std::abs(vec3.z));
 }
--- Maths.h
+++ Maths.h
@@ -2,6 +2,9 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <OGRE/OgreVector.h>
+#include <cmath>
+
 namespace SouthwestEngine {
 	class Math {
 	private:
@@ -9,10 +12,14 @@ namespace SouthwestEngine {
 	public:
 		static double Lerp(double v0, double v1, double t);
 		static double Cubic(double v[4], double t);
+		static float Distance(const Ogre::Vector3& p_from, const Ogre::Vector3& p_to);
 		static double MoveToward(double p_from, double p_to, double p_delta);
 		static float MoveToward(float p_from, float p_to, float p_delta);
-		// TODO - Add Ogre::Vector3 overloads
+		static Ogre::Vector3 MoveToward(const Ogre::Vector3& p_from, const Ogre::Vector3& p_to, const float p_delta);
+		static Ogre::Vector3 Abs(const Ogre::Vector3& vec3);
 	};
+
+	constexpr float CMP_EPSILON{ 0.00001f };
 }
 
 #endif
